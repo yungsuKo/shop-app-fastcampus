1c740bf6b744f6e5e27bc44adf27b8b6e6072a03
Tom Ko <yungsu2391@gmail.com>
2024-01-04 21:17:27 +0900

    add-product get router, view

--- src/routes/admin-products.router.js
+++ src/routes/admin-products.router.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const { checkAdmin } = require('../middlewares/auth');
+const Category = require('../models/categories.model');
 const router = express.Router();
 
+router.get('/add-product', checkAdmin,async(req, res) => {
+    try{
+        const categories = await Category.find();
+        res.render('admin/add-product', {
+            categories: categories
+        })
+    }catch(error){
+        console.log(error);
+        next(error);
+    }
+})
 
 module.exports = router;
