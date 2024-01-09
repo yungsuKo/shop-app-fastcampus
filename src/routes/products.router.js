40e3933ec1ce527f79c89c62f42a671be177772e
Tom Ko <yungsu2391@gmail.com>
2024-01-09 10:10:10 +0900

    상품 상세보기 UI 적용

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { getAllCategories } = require('../middlewares/product');
 const router = express.Router();
 const Product = require('../models/products.model');
+const fs = require('fs-extra');
 
 router.get('/', getAllCategories ,async (req, res, next) => {
     try{
         const products = await Product.find();
-        res.render('product', {
+        res.render('products', {
             products: products,
         });
     }catch(err){
@@ -16,4 +17,34 @@ router.get('/', getAllCategories ,async (req, res, next) => {
     
 })
 
+router.get('/:category', getAllCategories, async(req, res, next) => {
+    try{
+        const products = await Product.find({
+            category: req.params.category,
+        });
+        console.log(products);
+        res.render('products', {
+            products: products
+        });
+    } catch (err){
+        console.log(err);
+        next(err);
+    }
+})
+
+router.get('/:category/:product', async(req, res, next) => {
+    try{
+        const product = await Product.findOne({slug: req.params.product});
+        const galleryDir = 'src/public/product-images/'+product._id+'/gallery';
+        const galleryImages = await fs.readdir(galleryDir);
+
+        res.render('product', {
+            product,
+            galleryImages
+        })
+    }catch(err){
+        console.log(err);
+        next(err);
+    }
+})
 module.exports = router
