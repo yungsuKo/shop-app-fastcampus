428f45bf78a35c015d76acd2dd8d5a3f128fdfbb
Tom Ko <yungsu2391@gmail.com>
2024-01-11 21:21:53 +0900

    cart 갯수 조정, 비우기 api

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -47,6 +47,44 @@ router.post('/:productId', async(req, res, next) => {
         console.log(err);
         next(err);
     }
+});
+
+router.get('/update/:product', async (req, res, next) => {
+    const slug = req.params.product;
+    const action = req.query.action;
+    let cart = req.session.cart;
+    
+    for(let i = 0; i<cart.length; i++){
+        if(cart[i].title === slug){
+            console.log('activated');
+            switch(action){
+                case "add":
+                    cart[i].qty++;
+                    break;
+                case "remove":
+                    cart[i].qty--;
+                    break;
+                case "clear":
+                    cart.splice(i, 1);
+                    if(cart.length === 0){
+                        delete req.session.cart;
+                    }
+                    break;
+                default:
+                    console.log('update problem');
+                    break;
+            }
+        }
+        break;
+    }
+    req.flash('success', '장바구니가 업데이트되었습니다.')
+    res.redirect('back');
+})
+
+router.delete('/', async (req, res, next) => {
+    delete req.session.cart;
+    req.flash('success', '장바구니가 지워졌습니다.');
+    res.redirect('back');
 })
 
 module.exports = router;
